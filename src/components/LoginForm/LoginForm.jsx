5edc3bccf4bde33350add3fc29d7808ce1e8caef
unknown <orbbmax@gmail.com>
2019-11-20 20:21:03 +0100

    Separate component for login form

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useRef } from 'react';
+
+const LoginForm = () => {
+  const usernameInput = useRef();
+  const passwordInput = useRef();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const onSubmit = e => {
+    e.preventDefault();
+    const username = usernameInput.current;
+    const password = passwordInput.current;
+    if (username.value === 'admin' && password.value === 'admin') {
+      setIsLoggedIn(true);
+      return;
+    }
+    setIsLoggedIn(false);
+  };
+  return (
+    <>
+      <h2>Simple login form</h2>
+      <small>(Hint: try admin/admin)</small>
+      {isLoggedIn ? <p>User is logged in</p> : <p>User is logged out</p>}
+      <form className="login-form" onSubmit={onSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          className="login-form__input"
+          required
+          id="username"
+          type="text"
+          ref={usernameInput}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          className="login-form__input"
+          required
+          id="password"
+          type="password"
+          ref={passwordInput}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export { LoginForm };
